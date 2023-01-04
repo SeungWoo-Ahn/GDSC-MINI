7fa704f8dd71ab7763de3ca6b4c418956f6a5c55
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-01-04 19:19:34 +0900

    Fix/controller

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -2,22 +2,27 @@ import 'package:gdsc_mini_project/api/repository/api_repository.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
+  final _hasCodeAuth = false.obs;
+  final _isSignUp = false.obs;
+
+  bool get hasCodeAuth => _hasCodeAuth.value;
+  bool get isSignUp => _isSignUp.value;
+
   //번호 인증
   void phoneAuth({required String phoneNumber}) async {
     await ApiRepo().phoneAuth(phoneNumber: phoneNumber);
   }
 
   //코드 인증
-  Future<bool> codeAuth(
-      {required String phoneNumber, required String code}) async {
+  void codeAuth({required String phoneNumber, required String code}) async {
     final result =
         await ApiRepo().codeAuth(phoneNumber: phoneNumber, code: code);
-    return result;
+    _hasCodeAuth.value = result;
   }
 
   //회원 확인
-  Future<bool> checkSignUp({required String phoneNumber}) async {
+  void checkSignUp({required String phoneNumber}) async {
     final result = await ApiRepo().checkSignUp(phoneNumber: phoneNumber);
-    return result;
+    _isSignUp.value = result;
   }
 }
