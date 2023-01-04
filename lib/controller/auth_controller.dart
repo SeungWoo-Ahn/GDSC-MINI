1ec413ccb2483cf192ad989e8df9f5926987fde1
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-01-04 19:13:29 +0900

    Feat/controller 구성

---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,23 @@
+import 'package:gdsc_mini_project/api/repository/api_repository.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  //번호 인증
+  void phoneAuth({required String phoneNumber}) async {
+    await ApiRepo().phoneAuth(phoneNumber: phoneNumber);
+  }
+
+  //코드 인증
+  Future<bool> codeAuth(
+      {required String phoneNumber, required String code}) async {
+    final result =
+        await ApiRepo().codeAuth(phoneNumber: phoneNumber, code: code);
+    return result;
+  }
+
+  //회원 확인
+  Future<bool> checkSignUp({required String phoneNumber}) async {
+    final result = await ApiRepo().checkSignUp(phoneNumber: phoneNumber);
+    return result;
+  }
+}
